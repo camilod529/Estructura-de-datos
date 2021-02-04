545210484d49d08186cb870ed82df34ee56bf76e
chokpik <camilod529@gmail.com>
2021-02-04 11:13:55 -0500

    primmer commit

---
+++ c#/introduccionNET/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace introduccionNET
+{
+
+    class miNodo
+    {
+        public int valor;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            miNodo nuevoNodo = new miNodo();
+
+            nuevoNodo.valor = 128;
+
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("Me lamo Camilo \nSon las : "+ DateTime.Now);
+
+            Console.WriteLine("El valor almacenado en el nodo es: " + nuevoNodo.valor);
+        }
+    }
+}
