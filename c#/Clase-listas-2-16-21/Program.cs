974078ad647fd06d2e3ea1ef4321c516e7edb402
chokpik <camilod529@gmail.com>
2021-02-16 10:03:14 -0500

    creacion programa clase 2/16/21

---
+++ c#/Clase-listas-2-16-21/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Clase_listas_2_16_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
