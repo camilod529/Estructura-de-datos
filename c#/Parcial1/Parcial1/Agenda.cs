d2cb195f60f79f6fa074d32c91ea1fc146bc9aa8
chokpik <camilod529@gmail.com>
2021-03-23 12:17:29 -0500

    inicio del primer parcial

---
+++ c#/Parcial1/Parcial1/Agenda.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Parcial1
+{
+    class Agenda
+    {
+        string ClientId { get; set; }
+        string ClientName { get; set; }
+        string Email { get; set; }
+        string ClothingCode { get; set; }
+        string ClothingName { get; set; }
+        string Category { get; set; }
+        int QuantityToProduce { get; set; }
+        double UnitCost { get; set; }
+        int ProducedQuantity { get; set; }
+
+        public Agenda() { }
+
+        public Agenda(string ClientId, string ClientName, string Email, string ClothingCode, string ClothingName, string Category, int QuantityToProduce, double UnitCost, int ProducedQuantity)
+        {
+            this.ClientId = ClientId;
+            this.ClientName = ClientName;
+            this.Email = Email;
+            this.ClothingCode = ClothingCode;
+            this.ClothingName = ClothingName;
+            this.Category = Category;
+            this.QuantityToProduce = QuantityToProduce;
+            this.UnitCost = UnitCost;
+            this.ProducedQuantity = ProducedQuantity;
+        }
+
+
+
+        public void showAgendas(Dictionary<string, Agenda> dictionary)
+        {
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine("Numero de produccion: {0}", item.Key);
+                Console.WriteLine("ClientID: " + item.Value.ClientId + " Nombre Cliente: "+ item.Value.ClientName + " Email: " + item.Value.Email + " Codigo prenda: " + item.Value.ClothingCode + " Nombre prenda: " 
+                    + item.Value.ClothingName + " Categoria: " + item.Value.Category + " Cantidad a producir: " + item.Value.QuantityToProduce + " correo: " 
+                    + item.Value.UnitCost + " Cantidad Porducida: " + item.Value.ProducedQuantity);
+            }
+        }
+
+
+
+        public Dictionary<string, Agenda> loadData()
+        {
+            Dictionary<string, Agenda> dictionary = new Dictionary<string, Agenda>();
+            string path = @"./jsons/Agenda.json";
+            using (StreamReader jsonStream = File.OpenText(path))
+            {
+                var jsond = jsonStream.ReadToEnd();
+                dictionary = JsonConvert.DeserializeObject<Dictionary<string, Agenda>>(jsond);
+            }
+            return dictionary;
+        }
+
+        public void makingAgendas(Dictionary<string, Agenda> dictionary, string keyd, Agenda data)
+        {
+            if (!dictionary.ContainsKey(keyd))
+            {
+                dictionary.Add(keyd, data);
+                Console.WriteLine("Usuario creado");
+                string json = JsonConvert.SerializeObject(dictionary);
+                string path = @"./jsons/Agenda.json";
+                File.WriteAllText(path, json);
+                showAgendas(dictionary);
+
+            }
+            else
+            {
+                Console.WriteLine("Ya hay una agenda con ese Numero de produccion");
+            }
+        }
+    }
+}
---
+++ c#/Parcial1/Parcial1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Parcial1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Agenda> agendas = new Dictionary<string, Agenda>();
+            Agenda objAgenda = new Agenda();
+            //agendas = objAgenda.loadData();
+
+
+            //variables auxiliares
+            int option, QuantityToProduce, ProducedQuantity;
+            string productionNumber, ClientId, ClientName, Email, ClothingCode, ClothingName, Category;
+            double UnitCost; 
+            
+            do
+            {
+                Console.WriteLine("---Menú de Inicio---");
+                Console.WriteLine("Seleccione la acción que desea realizar:");
+                Console.WriteLine("1. Crear agendas de produccion");
+                Console.WriteLine("2. Actualizar cantidades fabricadas de las prendas");
+                Console.WriteLine("3. Consultar por categoria");
+                Console.WriteLine("4. Imprimir el costo de produccion de todas las agendas de produccion");
+                Console.WriteLine("5. Calcular el costo total de produccion");
+                Console.WriteLine("6. Calcular el total de produccion por cada categoria");
+                Console.WriteLine("7. salir");
+
+                option = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("\n\n");
+                switch(option)
+                {
+                    case 1:
+                        Console.WriteLine("Ingrese el numero de produccion de la agenda que desea crear");
+                        productionNumber = Console.ReadLine();
+                        Console.WriteLine("Ingrese el Id del cliente");
+
+                        objAgenda.makingAgendas(agendas, productionNumber, new Agenda());
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                    Console.WriteLine("Gracias por usar el programa");
+                        break;
+
+                    default:
+                        Console.WriteLine("Ingrese una opcion correcta");
+                        break;
+                }
+            } while (option != 7);
+            
+
+        }
+    }
+}
